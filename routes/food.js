a7c976979de2a62b6d32c11132166822ef79c366
Johann Moreno <johanson88@gmail.com>
2020-06-16 17:48:23 +0200

    add getfoods route

--- routes/food.js
+++ routes/food.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const Food = require('./../Models/Food');
 
 /* GET home page. */
 router.post('/', async (req, res, next) => {
-    console.log(req.body);
 
-    try {
-        const Food = require('./../Models/Food');
+    try {        
         const newFood = req.body;
         const foodSaved = await Food.create(newFood);
         res.status(202).json(foodSaved);
@@ -18,5 +17,20 @@ router.post('/', async (req, res, next) => {
 
     res.status(200).send();
 });
+
+router.get('/', async function(req, res, next) {
+
+    try {
+        const foodList = await Food.find();
+        //if (foodList == false) next(createError(404));
+        res.status(200).json(foodList);
+        //next(createError(404));
+    }
+    catch (error) {
+        next(error);
+    }
+});
+
+
 
 module.exports = router;
